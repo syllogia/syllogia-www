73a25beb727feee79f03eca5e480f7c7095a2e4a
Bamdad Sabbagh <git@bamdadsabbagh.com>
2020-06-22 23:16:56 +0200

    new readme

---
+++ README.md
@@ -0,0 +1,6 @@
+# [syllogia-www](https://syllogia.com/)
+
+## repositories
+
+- [gitlab](https://gitlab.com/syllogia/ressources/syllogia-www) main
+- [github](#)
---
+++ src/components/app/app.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import logo from './syllogia.svg'
+import './app.css'
+
+export default (): React.ReactElement => (
+    <div className="App">
+        <header className="App-header">
+            <img src={logo} className="App-logo" alt="logo" />
+        </header>
+    </div>
+)
