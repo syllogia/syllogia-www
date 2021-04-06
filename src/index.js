30441c9cce2e0cecbf6143a618271cf215cbc0a1
Bamdad Sabbagh <git@bamdadsabbagh.com>
2021-04-06 21:04:54 +0200

    feat(styles): moved to `sass-reset` npm package [force-release]

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import logo from './assets/syllogia.svg'
-import './styles/reset.scss'
+import 'sass-reset'
 import './styles/main.scss'
 
 const App = () => (
